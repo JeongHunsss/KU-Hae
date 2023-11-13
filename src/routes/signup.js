9f7ff3cad5159d922cfdf7b45b90e9234535f6c5
JeongHunPark <mizz1030@naver.com>
2023-11-13 17:11:52 +0900

    라우트 변경

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 
 // 회원가입 페이지 보기
 router.get('/', (req, res) => {
-  const filePath = path.join(__dirname, '../views/signup.html');
-  res.sendFile(filePath);
+  res.render('signup');
 });
-
 
 
 module.exports = router;
