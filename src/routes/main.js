2bd007a2bd037604d1c472e4492e3a8c7ee66621
JeongHunPark <mizz1030@naver.com>
2023-11-17 18:58:18 +0900

    세션 추가

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('main');
+  const user = req.session.user;
+  res.render('main', {user});
 });
 
   module.exports = router;
