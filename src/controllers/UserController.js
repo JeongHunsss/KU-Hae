448c3fee3553d5f3f562d7f89e5d0dcbe74b71ef
JeongHunPark <mizz1030@naver.com>
2023-11-20 17:35:55 +0900

    회원가입 로직 수정

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,12 +6,12 @@ const saltRounds = 10; // 솔트를 생성하는 데 사용되는 라운드 수
 exports.createUser = (req, res) => {
   // 유효성 검사
   if (!req.body.username || !req.body.password || !req.body.email) {
-    return res.render('signup', { message: '모든 필수 입력 항목을 채워주세요.' });
+    return res.status(200).json({ message: '모든 필수 입력 항목을 채워주세요.' });
   }
 
   // 비밀번호와 비밀번호 확인이 일치하는지 확인
-  if (req.body.password !== req.body['password-confirm']) {
-    return res.render('signup', { message: '비밀번호와 비밀번호 확인이 일치하지 않습니다.' });
+  if (req.body.password !== req.body.password_confirm) {
+    return res.status(200).json({ message: '비밀번호와 비밀번호 확인이 일치하지 않습니다.' });
   }
 
   // bcrypt를 사용하여 비밀번호 해시 생성
@@ -32,9 +32,9 @@ exports.createUser = (req, res) => {
     user.createUser(userData, (err, result) => {
       if (err) {
         console.error('회원가입 오류:', err);
-        return res.render('signup', { message: '회원가입 실패', details: err.message });
+        return res.status(500).json({ message: '회원가입 실패', details: err.message });
       } else {
-        return res.render('signup', { message: '회원가입 성공', isOK: 'OK' });
+        return res.status(200).json({ message: '회원가입 성공', isOK: true });
       }
     });
   });
@@ -45,17 +45,16 @@ exports.checkDuplicateUsername = (req, res) => {
 
   const user = new User();
   user.getUserById(username, (err, result) => {
-      if (err) {
-          console.error('아이디 중복 확인 오류:', err);
-          res.status(500).json({ error: '아이디 중복 확인 오류' });
+    if (err) {
+      console.error('아이디 중복 확인 오류:', err);
+      res.status(500).json({ error: '아이디 중복 확인 오류' });
+    } else {
+      if (result.length > 0) {
+        res.status(200).json({ isDuplicate: false, DupMessage: '이미 사용 중인 아이디입니다.' });
       } else {
-          console.log(result.length);
-          if (result.length > 0) {
-              res.json({ isDuplicate: 0, DupMessage: '이미 사용 중인 아이디입니다.' });
-          } else {
-              res.json({ isDuplicate: 1, DupMessage: '사용 가능한 아이디입니다.' });
-          }
+        res.status(200).json({ isDuplicate: true, DupMessage: '사용 가능한 아이디입니다.' });
       }
+    }
   });
 };
 
