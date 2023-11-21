2ecd5d058c7df9f1ca53a11bc6080bbb43593d69
JeongHunPark <mizz1030@naver.com>
2023-11-21 21:16:31 +0900

    report_log 모델

---
+++ src/models/Report_log.js
@@ -0,0 +1,22 @@
+const connection = require('../../config/DBconfig');
+
+class Report_log {
+    // 로그 생성
+  createLog(reportLogData, callback) {
+    const query = 'INSERT INTO report_log SET ?';
+    connection.query(query, reportLogData, callback);
+  }
+
+    // 특정 로그 가져오기
+  getListById(userId, postId, callback) {
+    const query = 'SELECT * FROM report_log WHERE user_id = ? AND post_id = ?';
+    connection.query(query, [userId, postId], callback);
+  }
+
+  // 연결 종료
+  closeConnection() {
+    connection.end();
+  }
+}
+
+module.exports = Report_log;
