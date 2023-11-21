3c47d7830c71dddca49f7b1632191497874c8a9b
JeongHunPark <mizz1030@naver.com>
2023-11-21 21:15:18 +0900

    ku_list 컨트롤러

--- src/controllers/KU_listController.js
+++ src/controllers/KU_listController.js
@@ -1,4 +1,5 @@
 const KU_list = require('../models/KU_list');
+const Report_log = require('../models/Report_log');
 
 // 모든 글 정보 가져오기
 exports.PageOpen = (req, res) => {
@@ -14,3 +15,36 @@ exports.PageOpen = (req, res) => {
       }
     });
   };
+
+// 신고하기
+exports.ReportList = (req, res) => {
+  const ku_list = new KU_list();
+  const report_log = new Report_log();
+  
+  const reportData = {
+    userId: req.body.userId,
+    postId: req.body.postId
+  };
+
+  report_log.getListById({ userId, postId }, (err, reportLog) => {
+    if (err) {
+      console.error('로그 가져오기 오류:', err);
+      res.status(500).json({ error: '로그 가져오기 실패' });
+    } else {
+      if (reportLog.length > 0) {
+        res.status(200).json({ message: '이미 신고한 글입니다.' });
+      } else {
+        report_log.createLog(reportData);
+        // 신고한 로그가 없으면, 신고 수를 증가시킴 (이는 예시로 신고 수를 올리는 로직입니다)
+        ku_list.increaseReportCount(postId, (increaseErr) => {
+          if (increaseErr) {
+            console.error('신고 수 증가 오류:', increaseErr);
+            res.status(500).json({ error: '신고 수 증가 실패' });
+          } else {
+            res.status(200).json({ message: '신고 완료' });
+          }
+        });
+      }
+    }
+  });
+};
