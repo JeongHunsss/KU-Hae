fa39618b6665cb76c1dcc403c3321d1a5c9987f3
JeongHunPark <mizz1030@naver.com>
2023-11-22 22:36:15 +0900

    세션 저장 방식 변경

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -11,8 +11,8 @@ exports.login = (req, res) => {
       console.error('로그인 오류:', err);
       res.status(500).send('로그인 실패');
     } else if (user) {
-      req.session.user = user.user_id;
-      console.log("(로그인)session: " + req.session.user);
+      req.session.user = user;
+      console.log("(로그인)session: " + req.session.user.user_id);
       res.redirect('/main'); // 로그인 성공 시 홈페이지로 리다이렉트
     } else {
       res.render('login', { message: '아이디 또는 비밀번호가 틀립니다.' });
