9976e9fc9f915720bc5e660ef8f162d4b52f3599
JeongHunPark <mizz1030@naver.com>
2023-11-25 15:48:22 +0900

    Chat 모델 추가

---
+++ src/models/Chat_request.js
@@ -0,0 +1,28 @@
+const connection = require('../../config/DBconfig');
+
+class User {
+    // 요청 생성
+  createRequest(requestData, callback) {
+    const query = 'INSERT INTO chat_request SET ?';
+    connection.query(query, requestData, callback);
+  }
+
+  // 요청 제거
+  deleteRequest(request_id, callback) {
+    const query = 'DELETE FROM chat_request WHERE id = ?';
+    connection.query(query, request_id, callback);
+  }
+
+  // 특정 유저의 요청 가져오기
+  getRequestById(userId, callback) {
+    const query = 'SELECT * FROM chat_request WHERE receiver = ?';
+    connection.query(query, [userId], callback);
+  }
+
+  // 연결 종료
+  closeConnection() {
+    connection.end();
+  }
+}
+
+module.exports = User;
