0cd3bd8a0501ee7fa9a8daab3a6c090dc2eec36b
JeongHunPark <mizz1030@naver.com>
2023-11-26 01:13:10 +0900

    페이지네이션 추가

--- src/models/Contest.js
+++ src/models/Contest.js
@@ -13,6 +13,30 @@ class Contest {
     connection.query(query, callback);
   }
 
+  //페이지네이션 (전체)
+  getContestPerPage(perPage, page, callback) {
+    const offset = (page - 1) * perPage; // 해당 페이지의 시작 오프셋
+  
+    const query = 'SELECT * FROM contest ORDER BY id DESC LIMIT ?, ?';
+    connection.query(query, [offset, perPage], callback);
+  }
+
+  //페이지네이션 (검색)
+  getContestByTitlePerPage(searchTitle, perPage, page, callback) {
+    const offset = (page - 1) * perPage; // 해당 페이지의 시작 오프셋
+  
+    const query = 'SELECT * FROM contest WHERE title LIKE ? ORDER BY id DESC LIMIT ?, ?';
+    const searchTerm = '%' + searchTitle + '%'
+    connection.query(query, [searchTerm, offset, perPage], callback);
+  }
+
+  // 특정 공모전 가져오기
+  getContestByTitle(searchTitle, callback) {
+    const query = 'SELECT * FROM contest WHERE title LIKE ?';
+    const searchTerm = '%' + searchTitle + '%'
+    connection.query(query, [searchTerm], callback);
+  }
+
   // 연결 종료
   closeConnection() {
     connection.end();
