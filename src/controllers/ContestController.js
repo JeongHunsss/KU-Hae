3aa2c78f5f4b45a20baf405f15d6efc7976d3d34
JeongHunPark <mizz1030@naver.com>
2023-11-26 01:12:59 +0900

    contest 컨트롤러 생성(페이지 오픈 추가)

---
+++ src/controllers/ContestController.js
@@ -0,0 +1,54 @@
+const Contest = require('../models/Contest');
+
+
+// 모든 글 정보 가져오기
+exports.PageOpen = (req, res) => {
+  const perPage = 6; // 페이지당 아이템 수
+  const page = req.query.page || 1; // 페이지 번호, 기본값은 1
+  const searchTitle = req.query.searchTitle;
+  
+  const contest = new Contest();
+
+  // 검색X (전체 글)
+  if(!searchTitle) {
+    contest.getAllContest((err, result) => {
+      if (err) {
+        console.error('리스트 불러오기 오류:', err);
+        res.status(500).json({ error: '리스트 불러오기 오류' });
+      } else {
+        const totalList = result.length;
+        // 페이지네이션 페이지 수 계산
+        const totalPages = Math.ceil(totalList / perPage);
+        
+        contest.getContestPerPage(perPage, page, (err, lists) => {
+          if (err) {
+            console.error('글 정보 가져오기 오류:', err);
+            res.status(500).json({ error: '글 정보 가져오기 실패' });
+          } else {
+            res.render('contest_list', {lists, user: req.session.user, rooms: req.session.rooms, totalPages, searchTitle: '전체'});
+          }
+        });
+      }
+    });
+  } else {  //검색O (특정 카테고리)
+    contest.getContestByTitle(searchTitle, (err, result) => {
+      if (err) {
+        console.error('리스트 불러오기 오류:', err);
+        res.status(500).json({ error: '리스트 불러오기 오류' });
+      } else {
+        const totalList = result.length;
+        // 페이지네이션 페이지 수 계산
+        const totalPages = Math.ceil(totalList / perPage);
+        
+        contest.getContestByTitlePerPage(searchTitle, perPage, page, (err, lists) => {
+          if (err) {
+            console.error('글 정보 가져오기 오류:', err);
+            res.status(500).json({ error: '글 정보 가져오기 실패' });
+          } else {
+            res.render('contest_list', {lists, user: req.session.user, rooms: req.session.rooms, totalPages, searchTitle});
+          }
+        });
+      }
+    });
+  }
+};
