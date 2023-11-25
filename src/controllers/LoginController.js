3a66bfad6f992ccb24e645efad9fcd8fcf3835da
JeongHunPark <mizz1030@naver.com>
2023-11-25 23:32:30 +0900

    채팅방 가져오기 추가

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Chat_rooms = require('../models/Chat_rooms');
 
 // 로그인
 exports.login = (req, res) => {
@@ -12,8 +13,20 @@ exports.login = (req, res) => {
       res.status(500).send('로그인 실패');
     } else if (user) {
       req.session.user = user;
-      console.log("(로그인)session: " + req.session.user.user_id);
-      res.redirect('/main'); // 로그인 성공 시 홈페이지로 리다이렉트
+
+      // 유저 채팅방 목록 가져오기
+      const chat_rooms = new Chat_rooms();
+
+      chat_rooms.getRoomByuserId(req.session.user.user_id, (err, rooms) => {
+        if (err) {
+          console.error('채팅방 가져오기 오류:', err);
+          return res.status(500).json({ message: '채팅방 가져오기 실패', details: err.message });
+        } else {
+          req.session.rooms = rooms;
+          console.log("(로그인)session: " + req.session.user.user_id);
+          res.redirect('/main'); // 로그인 성공 시 홈페이지로 리다이렉트
+        }
+      });
     } else {
       res.render('login', { message: '아이디 또는 비밀번호가 틀립니다.' });
     }
