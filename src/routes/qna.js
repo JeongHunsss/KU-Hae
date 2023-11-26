6b86247927ebdbb55d3f179846dff7c3310bde92
JeongHunPark <mizz1030@naver.com>
2023-11-26 23:57:12 +0900

    qna 라우팅 추가

--- src/routes/qna.js
+++ src/routes/qna.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('qna', {user: req.session.user, rooms: req.session.rooms});
-});
+const qnaController = require('../controllers/QnAController');
+
+router.get('/', qnaController.PageOpen);
 
 router.get('/add', (req, res) => {
   res.render('qna_add', {user: req.session.user, rooms: req.session.rooms});
 });
 
-router.get('/detail', (req, res) => {
-  res.render('qna_detail', {user: req.session.user, rooms: req.session.rooms});
-});
+router.get('/detail', qnaController.ViewDetail);
+
+router.post('/addqestion', qnaController.AddQestion);
+router.post('/deletequestion', qnaController.DeleteQuestion);
+
+router.post('/addanswer', qnaController.AddAnswer);
+router.post('/editanswer', qnaController.EditAnswer);
+router.post('/deleteanswer', qnaController.DeleteAnswer);
 
   module.exports = router;
